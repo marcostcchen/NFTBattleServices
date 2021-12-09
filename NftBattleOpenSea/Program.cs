87330931632d3f18b061b799479457cfc98bac46
Marcos Chen <marcostcchen@gmail.com>
2021-12-09 16:57:45 -0300

    Api - fetch program

--- NftBattleOpenSea/Program.cs
+++ NftBattleOpenSea/Program.cs
@@ -1,95 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text.Json;
 
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
-using System.Text.Json;
-
-using (var client = new HttpClient())
+//Funcao da chamada
+async Task<List<Nft>> getUserNfts()
 {
-    client.BaseAddress = new Uri("https://api.opensea.io/api/v1");
-
-    var parameters = "/assets?order_direction=desc&offset=0&limit=5&collection=ethermon";
-    var result = await client.GetAsync(parameters).ConfigureAwait(false);
-
-    if (!result.IsSuccessStatusCode) throw new Exception(result.ReasonPhrase);
-    var content = await result.Content.ReadAsStringAsync().ConfigureAwait(false);
-
-    var serializerOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-    var nftAssets = JsonSerializer.Deserialize<GetAssetResponse>(content, serializerOptions)?.Assets ?? new List<NftAsset>();
-
-    var nfts = new List<Nft>();
-
-    nftAssets.ForEach(asset =>
+    using (var client = new HttpClient())
     {
-        nfts.Add(new Nft()
-        {
-            Token_id = asset.Token_id,
-            Image_url = asset.Image_url,
-            Name = asset.Name,
-            Owner = null,
-            //Owner = new Owner()
-            //{
-            //    Name = asset.Owner?.User.Username
-            //},
-            Attack = new Random().Next(0, 100),
-            Defence = new Random().Next(0, 100),
-            Health = new Random().Next(0, 100),
-        });
-    });
+        var parameters = "/usernft";
+        var token = "eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiI2MTkyYTA4MDNiNGJlYjFlMmIzNTVmMTQiLCJleHAiOjE2MzkxMDc1MTcsImlzcyI6ImJlYWNvbnRyYWNrZXIuY29tLmJyIiwiYXVkIjoiYmVhY29udHJhY2tlci5jb20uYnIifQ.3pi7i5VisFBsmpjItd-ZUXxzt1QiskY-oN-djnXT4L4";
+        var Auth = "Bearer " + token;
+        
+        client.BaseAddress = new Uri("https://nftbattleapi.beacontracker.software");
+        client.DefaultRequestHeaders.Add("Authorization", Auth);
+        
+        var result = await client.GetAsync(parameters).ConfigureAwait(false);
 
-    Console.Write(JsonSerializer.Serialize(nfts));
+        if (!result.IsSuccessStatusCode) throw new Exception(result.ReasonPhrase);
+        var content = await result.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+        var serializerOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        var nfts = JsonSerializer.Deserialize<List<Nft>>(content, serializerOptions) ?? new List<Nft>();
+
+        return nfts;
+    }
 }
 
-public class GetAssetResponse
+//Chamada teste
+var nfts = getUserNfts().Result;
+foreach (var nft in nfts)
 {
-    public List<NftAsset> Assets { get; set; }
-}
-public class NftAsset
-{
-    public string Token_id { get; set; }
-    public string Image_url { get; set; }
-    public string Name { get; set; }
-    public string External_link { get; set; }
-    public OpenSeaOwner? Owner { get; set; }
-    public List<Traits> Traits { get; set; }
+    Console.WriteLine(nft.Name);
 }
 
-public class Traits
-{
-    public string? Trait_type { get; set; }
-    public dynamic? Value { get; set; }
-    public string? Display_type { get; set; }
-}
-
-public class OpenSeaOwner
-{
-    public OpenSeaUser User { get; set; }
-    public string Profile_img_url { get; set; }
-}
-
-public class OpenSeaUser
-{
-    public string Username { get; set; }
-}
-
+//Classe Nft
 public class Nft
 {
-    [BsonId]
-    [BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; }
     public string Token_id { get; set; }
     public string Image_url { get; set; }
     public string Name { get; set; }
-    public Owner Owner { get; set; }
     public int Attack { get; set; }
     public int Defence { get; set; }
     public int Health { get; set; }
-}
-
-public class Owner
-{
-    [BsonId]
-    [BsonRepresentation(BsonType.ObjectId)]
-    public string Id { get; set; }
-    public string Name { get; set; }
 }
